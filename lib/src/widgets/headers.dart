f42d05f37ef90fc63f4aafd8ff65a6fd25e6e2b0
Brian Vega <dentvega6@gmail.com>
2020-09-06 09:02:32 -0400

    Header Pico

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -114,3 +114,43 @@ class _HeaderTriangularPainter extends CustomPainter {
     return true;
   }
 }
+
+//Header Pico
+
+class HeaderPico extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // color: Color(0Xff615ABB),
+      child: CustomPaint(
+        painter: _HeaderPicoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+    lapiz.color = Color(0Xff615ABB);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 2;
+    final path = new Path();
+
+    path.lineTo(0, size.height * 0.2);
+    path.lineTo(size.width * 0.5, size.height * 0.28);
+    path.lineTo(size.width, size.height * 0.20);
+    path.lineTo(size.width, 0);
+
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
