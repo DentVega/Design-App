ba7ee82f07097d2044c8e370bef616083f9c230c
Brian Vega <dentvega6@gmail.com>
2020-09-06 10:28:46 -0400

    - Preparando seccion de animaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:design_app/src/pages/headers_page.dart';
+import 'package:design_app/src/pages/animaciones_page.dart';
+// import 'package:design_app/src/pages/headers_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
-      home: HeadersPage(),
+      home: AnimacionesPage(),
     );
   }
 }
---
+++ lib/src/pages/animaciones_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AnimacionesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CuadradoAnimado(),
+      ),
+    );
+  }
+}
+
+class CuadradoAnimado extends StatefulWidget {
+  const CuadradoAnimado({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangle();
+  }
+}
+
+class _Rectangle extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+    return Container(
+       width: 70,
+       height: 70,
+       decoration: BoxDecoration(
+         color: Colors.blue
+       ),
+     );
+   }
+}
