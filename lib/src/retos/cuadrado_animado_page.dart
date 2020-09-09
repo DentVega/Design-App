465e4bb5877cc6e3728c7882cbf389e2d8699412
Brian Vega <dentvega6@gmail.com>
2020-09-09 07:30:07 -0400

    - Reto y solucion

--- lib/src/retos/cuadrado_animado_page.dart
+++ lib/src/retos/cuadrado_animado_page.dart
@@ -18,14 +18,37 @@ class _CuadradoAnimado extends StatefulWidget {
   __CuadradoAnimadoState createState() => __CuadradoAnimadoState();
 }
 
-class __CuadradoAnimadoState extends State<_CuadradoAnimado> {
-
+class __CuadradoAnimadoState extends State<_CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
   AnimationController animationController;
+
   //Animaciones
+  Animation<double> moverDerecha;
+  Animation<double> moverArriba;
+  Animation<double> moverIzquierda;
+  Animation<double> moverAbajo;
 
   @override
   void initState() {
-    // TODO: implement initState
+    animationController = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 4000));
+
+    moverDerecha = Tween(begin: 0.0, end: 150.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.0, 0.25, curve: Curves.bounceOut)));
+
+    moverArriba = Tween(begin: 0.0, end: -150.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.25, 0.50, curve: Curves.bounceOut)));
+
+    moverIzquierda = Tween(begin: 150.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.50, 0.75, curve: Curves.bounceOut)));
+
+    moverAbajo = Tween(begin: -150.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.75, 1.0, curve: Curves.bounceOut)));
+
     super.initState();
   }
 
@@ -37,19 +60,35 @@ class __CuadradoAnimadoState extends State<_CuadradoAnimado> {
 
   @override
   Widget build(BuildContext context) {
-    return _Rectangle();
+    animationController.forward();
+
+    return AnimatedBuilder(
+      animation: animationController,
+      child: _Rectangle(),
+      builder: (BuildContext context, Widget child) {
+        print('${moverIzquierda.value}');
+        return Transform.translate(
+          offset: Offset(
+              moverArriba.value != -150
+                  ? moverDerecha.value
+                  : moverIzquierda.value,
+              moverIzquierda.value != 0.0
+                  ? moverArriba.value
+                  : moverAbajo.value),
+          child: child,
+        );
+      },
+    );
   }
 }
 
 class _Rectangle extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-       width: 70,
-       height: 70,
-       decoration: BoxDecoration(
-         color: Colors.blue
-       ),
-     );
-   }
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(color: Colors.blue),
+    );
+  }
 }
