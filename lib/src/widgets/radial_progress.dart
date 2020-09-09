e63213bd24782f6425fa7d44e3ff6bd77eeb8aea
Brian Vega <dentvega6@gmail.com>
2020-09-09 10:02:25 -0400

    - Dibujar el circulo y arco inicial

--- lib/src/widgets/radial_progress.dart
+++ lib/src/widgets/radial_progress.dart
@@ -1,7 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RadialProgress extends StatefulWidget {
-
   final porcentaje;
 
   RadialProgress({this.porcentaje});
@@ -11,11 +12,48 @@ class RadialProgress extends StatefulWidget {
 }
 
 class _RadialProgressState extends State<RadialProgress> {
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('${widget.porcentaje}'),
+      padding: EdgeInsets.all(10),
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _MiRadialProgress(widget.porcentaje),
+      ),
     );
   }
+}
+
+class _MiRadialProgress extends CustomPainter {
+  final porcentaje;
+
+  _MiRadialProgress(this.porcentaje);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint()
+      ..strokeWidth = 4
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+
+    Offset center = Offset(size.width * 0.5, size.height * 0.5);
+    double radio = min(size.width * 0.5, size.height * 0.5);
+
+    canvas.drawCircle(center, radio, paint);
+
+    //Arco
+    final paintArco = new Paint()
+      ..strokeWidth = 10
+      ..color = Colors.pink
+      ..style = PaintingStyle.stroke;
+
+    //Parte que se debera ir llenando
+    double arcAngulo = 2 * pi * (porcentaje / 100);
+    canvas.drawArc(Rect.fromCircle(center: center, radius: radio), -pi / 2,
+        arcAngulo, false, paintArco);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
