31fdd47b15873516b4e401a2aed3905e786b3d10
Brian Vega <dentvega6@gmail.com>
2020-09-14 09:06:29 -0400

    Emergency Layout - IconHeader

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,12 +1,11 @@
+import 'package:design_app/src/widgets/icon_header.dart';
 import 'package:flutter/material.dart';
 
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
+      body: IconHeader(),
     );
   }
 }
