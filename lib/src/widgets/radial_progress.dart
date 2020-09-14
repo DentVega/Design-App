046ca75b708833c8bb531bbba5b4589a0b255ea2
Brian Vega <dentvega6@gmail.com>
2020-09-13 20:08:56 -0400

    - Animando mi Radial Progress Widget

--- lib/src/widgets/radial_progress.dart
+++ lib/src/widgets/radial_progress.dart
@@ -11,17 +11,47 @@ class RadialProgress extends StatefulWidget {
   _RadialProgressState createState() => _RadialProgressState();
 }
 
-class _RadialProgressState extends State<RadialProgress> {
+class _RadialProgressState extends State<RadialProgress>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  double porcentajeAnterior;
+
+  @override
+  void initState() {
+    porcentajeAnterior = widget.porcentaje;
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      width: double.infinity,
-      height: double.infinity,
-      child: CustomPaint(
-        painter: _MiRadialProgress(widget.porcentaje),
-      ),
-    );
+    animationController.forward(from: 0.0);
+
+    final diferenciaAnimar = widget.porcentaje - porcentajeAnterior;
+    porcentajeAnterior = widget.porcentaje;
+
+    return AnimatedBuilder(
+        animation: animationController,
+        builder: (BuildContext context, Widget child) {
+          return Container(
+            padding: EdgeInsets.all(10),
+            width: double.infinity,
+            height: double.infinity,
+            child: CustomPaint(
+              painter: _MiRadialProgress(
+                  (widget.porcentaje - diferenciaAnimar) +
+                      (diferenciaAnimar * animationController.value)),
+            ),
+          );
+        });
   }
 }
 
