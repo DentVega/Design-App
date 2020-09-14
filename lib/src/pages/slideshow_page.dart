ff3bcbfe1f860dd036c4a9805c8ed2c089a15938
Brian Vega <dentvega6@gmail.com>
2020-09-14 04:11:04 -0400

    SlidesShow - Colocando dos slideshows

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -7,19 +7,35 @@ class SlideshowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Slidesshow(
-        puntosArriba: false,
-        colorPrimario: Colors.red,
-        bulletPrimario: 15,
-        bulletSecundario: 10,
-        slides: [
-          SvgPicture.asset('assets/svgs/slide-1.svg'),
-          SvgPicture.asset('assets/svgs/slide-2.svg'),
-          SvgPicture.asset('assets/svgs/slide-3.svg'),
-          SvgPicture.asset('assets/svgs/slide-4.svg'),
-          SvgPicture.asset('assets/svgs/slide-5.svg'),
+      body: Column(
+        children: [
+          Expanded(child: MiSlideshow()),
+          Expanded(child: MiSlideshow())
         ],
       ),
+    );
+  }
+}
+
+class MiSlideshow extends StatelessWidget {
+  const MiSlideshow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidesshow(
+      puntosArriba: false,
+      colorPrimario: Colors.red,
+      bulletPrimario: 15,
+      bulletSecundario: 10,
+      slides: [
+        SvgPicture.asset('assets/svgs/slide-1.svg'),
+        SvgPicture.asset('assets/svgs/slide-2.svg'),
+        SvgPicture.asset('assets/svgs/slide-3.svg'),
+        SvgPicture.asset('assets/svgs/slide-4.svg'),
+        SvgPicture.asset('assets/svgs/slide-5.svg'),
+      ],
     );
   }
 }
