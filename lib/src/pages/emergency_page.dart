fec84a50fd28d7913dac621087612cb602e50949
Brian Vega <dentvega6@gmail.com>
2020-09-14 09:52:59 -0400

    Emergency Layout - Creando los botones

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,3 +1,4 @@
+import 'package:design_app/src/widgets/boton_gordo.dart';
 import 'package:design_app/src/widgets/icon_header.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -6,13 +7,22 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(
-        icon: FontAwesomeIcons.plus,
-        titulo: 'Asistencia Medica',
-        subTitulo: 'Haz solicitado',
-        color1: Color(0xff526BF6),
-        color2: Color(0xff67ACF2),
+      body: Center(
+        child: BotonGordo(),
       ),
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+      icon: FontAwesomeIcons.plus,
+      titulo: 'Asistencia Medica',
+      subTitulo: 'Haz solicitado',
+      color1: Color(0xff526BF6),
+      color2: Color(0xff67ACF2),
     );
   }
 }
